c0be1f0b42336f372bf7a00a00c5cb6053fce8d4
Reddy Velagala <narayana.reddy-velagala@sogeti.com>
2019-04-07 23:24:49 +0200

    This is my first commit

---
+++ src/test/java/com/practice/TestNGExample/SampleTestOne.java
@@ -0,0 +1,10 @@
+package com.practice.TestNGExample;
+
+import org.testng.annotations.Test;
+
+public class SampleTestOne {
+   @Test
+   public void TestMethod() {
+	   System.out.print("this is starting project");
+   }
+}
