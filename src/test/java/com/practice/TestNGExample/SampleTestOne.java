b9f46c78e362821ed36285246300048acd4433eb
Reddy Velagala <narayana.reddy-velagala@sogeti.com>
2019-04-08 15:50:11 +0200

    this is second commit

--- src/test/java/com/practice/TestNGExample/SampleTestOne.java
+++ src/test/java/com/practice/TestNGExample/SampleTestOne.java
@@ -1,10 +1,35 @@
 package com.practice.TestNGExample;
 
+import static org.testng.Assert.assertEquals;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class SampleTestOne {
    @Test
    public void TestMethod() {
-	   System.out.print("this is starting project");
+	   System.setProperty("webdriver.gecko.driver", "C:\\\\Users\\nreddyve\\Desktop\\Jenkins\\geckodriver.exe");
+	   WebDriver driver = new FirefoxDriver();
+	   driver.get("https://www.amazon.in/");
+	   driver.findElement(By.xpath("//*[@id=\"nav-your-amazon\"]")).click();
+	   driver.findElement(By.xpath("//*[@id=\"ap_email\"]")).sendKeys("dvnreddy.v@gmail.com");
+	   driver.findElement(By.xpath("//*[@id=\"continue\"]")).click();
+	   driver.findElement(By.xpath(" //*[@id=\"ap_password\"]")).sendKeys("dvnreddy.v@gmail.com");
+	   driver.findElement(By.xpath("//*[@id=\"a-autoid-0\"]")).click();
+	   try {
+		   String actualTitle = driver.getTitle();
+		   String expectedTitle = "Amazon Sign In";
+		   assertEquals(expectedTitle,actualTitle);
+		   System.out.println("PASS");
+
+		} catch (Exception e) {
+			System.out.println("FAIL");
+		}
+	 
+	   
+	   
    }
 }
